9d2f39c56004083a761e01b0c591e4962964d1ed
basit-shafiqq <basitshafiq972@gmail.com>
2024-09-02 15:22:28 +0530

    changed the logo

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -12,7 +12,11 @@ function Header() {
     },[]);
   return (
     <div className='flex p-6 items-center justify-between bg-secondary shadow-md'>
-        <Image src={logo} width={160} height={100} alt='Logo'/>
+        <div className="flex items-center gap-4 p-4 bg-gray-100 rounded-lg shadow-md">
+  <Image src={logo} width={40} height={80} alt="Logo" className="rounded-md"/>
+  <h2 className="text-3xl font-bold text-gray-800">InterVuAi</h2>
+</div>
+
         <ul className='hidden md:flex gap-8'>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/faqs' && 'text-primary font-bold'}`}>FAQs</li>
